7d0b81fad6edfd55708e0ef81f61144968a2b4c3
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-03-18 20:38:07 +1000

    Update quarto files

--- test.R
+++ test.R
@@ -1,3 +1,48 @@
+# Test the metadata-files
+
+file.copy(
+    "_quarto1.yml",
+    "_quarto.yml",
+    overwrite = TRUE
+)
+
+
+system.time({
+    quarto::quarto_render(
+        "index.qmd",
+        quarto_args = c(
+            "--log-level", "debug"
+        )
+    )
+})
+
+
+# Test without the metadata-file
+file.copy(
+    "_quarto2.yml",
+    "_quarto.yml",
+    overwrite = TRUE
+)
+
+
+system.time({
+    quarto::quarto_render(
+        "index.qmd",
+        quarto_args = c(
+            "--log-level", "debug"
+        )
+    )
+})
+
+
+# Test with the metadata-file and without rscript
+file.copy(
+    "_quarto3.yml",
+    "_quarto.yml",
+    overwrite = TRUE
+)
+
+
 system.time({
     quarto::quarto_render(
         "index.qmd",
