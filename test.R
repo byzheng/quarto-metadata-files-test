c91cd6f616f2925f76390e08c14e37f51c8f82ab
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-03-18 20:50:44 +1000

    create actions

--- test.R
+++ test.R
@@ -1,55 +1,73 @@
-# Test the metadata-files
+Sys.setenv(QUARTO_KNITR_RSCRIPT_ARGS="--vanilla")
 
-file.copy(
-    "_quarto1.yml",
-    "_quarto.yml",
-    overwrite = TRUE
+if (!requireNamespace("quarto", quietly = TRUE)) {
+    stop("The 'quarto' package must be installed before running test.R", call. = FALSE)
+}
+
+results_dir <- Sys.getenv("RESULTS_DIR", unset = "")
+
+if (nzchar(results_dir)) {
+    dir.create(results_dir, recursive = TRUE, showWarnings = FALSE)
+}
+
+cleanup <- function() {
+    if (file.exists("_quarto.yml")) {
+        file.remove("_quarto.yml")
+    }
+}
+
+on.exit(cleanup(), add = TRUE)
+
+scenarios <- list(
+    list(name = "metadata_file", config = "_quarto1.yml"),
+    list(name = "inline_metadata", config = "_quarto2.yml"),
+    list(name = "metadata_file_render_list", config = "_quarto3.yml")
 )
 
+results <- vector("list", length(scenarios))
 
-system.time({
-    quarto::quarto_render(
-        "index.qmd",
-        quarto_args = c(
-            "--log-level", "debug"
-        )
+for (index in seq_along(scenarios)) {
+    scenario <- scenarios[[index]]
+
+    file.copy(
+        scenario$config,
+        "_quarto.yml",
+        overwrite = TRUE
     )
-})
 
-
-# Test without the metadata-file
-file.copy(
-    "_quarto2.yml",
-    "_quarto.yml",
-    overwrite = TRUE
-)
-
-
-system.time({
-    quarto::quarto_render(
-        "index.qmd",
-        quarto_args = c(
-            "--log-level", "debug"
+    timing <- system.time({
+        quarto::quarto_render(
+            "index.qmd",
+            quarto_args = c(
+                "--log-level", "debug"
+            )
         )
+    })
+
+    scenario_result <- data.frame(
+        scenario = scenario$name,
+        config = scenario$config,
+        user = unname(timing[["user.self"]]),
+        system = unname(timing[["sys.self"]]),
+        elapsed = unname(timing[["elapsed"]]),
+        stringsAsFactors = FALSE
     )
-})
 
+    print(scenario_result, row.names = FALSE)
+    results[[index]] <- scenario_result
+}
 
-# Test with the metadata-file and without rscript
-file.copy(
-    "_quarto3.yml",
-    "_quarto.yml",
-    overwrite = TRUE
-)
+results_df <- do.call(rbind, results)
 
+cat("\nCombined timings\n")
+print(results_df, row.names = FALSE)
 
-system.time({
-    quarto::quarto_render(
-        "index.qmd",
-        quarto_args = c(
-            "--log-level", "debug"
-        )
+if (nzchar(results_dir)) {
+    write.csv(
+        results_df,
+        file.path(results_dir, "timings.csv"),
+        row.names = FALSE
     )
-})
+}
 
-file.remove("_quarto.yml")
+
